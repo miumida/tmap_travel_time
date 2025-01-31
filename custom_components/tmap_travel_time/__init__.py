b22bfa11b7a7d8d778ffd5bdd96117789f9cc6c1
af950833 <af950833@hanmail.net>
2025-01-31 20:11:52 +0900

    Create __init__.py

---
+++ custom_components/tmap_travel_time/__init__.py
@@ -0,0 +1,30 @@
+"""Tmap Travel Time Integration."""
+from __future__ import annotations
+
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .config_flow import TmapTravelTimeConfigFlow  # noqa: F401
+from .sensor import async_setup_entry
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "tmap_travel_time"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the component."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up from config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return True
+  
