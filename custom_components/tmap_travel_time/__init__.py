62235bda0b7df74a9d208dc5950a880ebf8416a8
af950833 <af950833@hanmail.net>
2025-02-01 09:18:34 +0900

    Update __init__.py

--- custom_components/tmap_travel_time/__init__.py
+++ custom_components/tmap_travel_time/__init__.py
@@ -18,11 +18,11 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up from config entry."""
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     return True
-  
+    
