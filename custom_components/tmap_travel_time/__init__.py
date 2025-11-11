d360b1d5749e178e860ccc6db7eb6ef829338c24
miumida <46196137+miumida@users.noreply.github.com>
2025-11-12 01:04:11 +0900

    Update __init__.py

--- custom_components/tmap_travel_time/__init__.py
+++ custom_components/tmap_travel_time/__init__.py
@@ -3,7 +3,13 @@ from __future__ import annotations
 
 import logging
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.core import (
+    HomeAssistant,
+    ServiceResponse,
+    ServiceCall,
+    SupportsResponse,
+)
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .config_flow import TmapTravelTimeConfigFlow  # noqa: F401
 from .sensor import async_setup_entry
@@ -19,6 +25,9 @@ async def async_setup(hass: HomeAssistant, config: dict):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up from config entry."""
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+
+    await _async_setup_service(hass, entry)
+
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -26,3 +35,47 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     return True
     
+async def _async_setup_service( hass: HomeAssistant, entry: ConfigEntry ) -> None:
+    """서비스를 설정합니다."""
+
+    session = async_get_clientsession(hass)
+
+    async def _async_routes(call: ServiceCall) -> None:
+
+        start_x = call.data.get("start_x", None)
+        start_y = call.data.get("start_y", None)
+        end_x = call.data.get("end_x", None)
+        end_y = call.data.get("end_y", None)
+        req_coord_type = call.data.get("req_coord_type", None)
+
+        if not start_x or not start_y or not end_x or not end_y:
+            _LOGGER.error(f"[{DOMAIN}] _async_routes API Error: {result.get('errorMessage', 'Unknown error')}")
+
+        # API request configuration
+        headers = {
+            "appKey": entry.data["api_key"],
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+        
+        payload = {
+            "startX": start_x,
+            "startY": start_y,
+            "endX": end_x,
+            "endY": end_y,
+            "reqCoordType": req_coord_type,
+            "version": 1
+        }
+
+        response = await session.post("https://apis.openapi.sk.com/tmap/routes", headers=headers, json=payload)
+        
+        result = await response.json()
+
+        if response.status != 200:
+            _LOGGER.error(f"[{DOMAIN}] _async_routes API Error: {result.get('errorMessage', 'Unknown error')}")
+
+        total_time = result["features"][0]["properties"]["totalTime"]
+        
+        return result["features"][0]["properties"]
+
+    hass.services.async_register(DOMAIN, "routes", _async_routes, supports_response=SupportsResponse.ONLY, )
